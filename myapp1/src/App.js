1ac8d2c834dbf8d83e90be3268bdec0bdef9dda0
Atharva Yemul <atharvayemul04@gmail.com>
2024-01-30 16:19:37 +0530

    app1 Finished

--- myapp1/src/App.js
+++ myapp1/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+let name = "Athara";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with me
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Abot</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis velit accusamus fuga, voluptate laborum unde commodi molestias ad modi quibusdam minus porro nostrum doloribus! Ducimus suscipit odio quasi cupiditate officia? Odio quaerat ipsum atque!</p>
     </div>
+   </>
   );
 }
 
